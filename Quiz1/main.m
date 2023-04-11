e8eceeb87d8dd33f434e8e39aa58bb0bb134d1d1
Calalari <68549106+Calalari@users.noreply.github.com>
2023-04-11 14:23:52 +0300

    Update main.m

--- Quiz1/main.m
+++ Quiz1/main.m
@@ -70,10 +70,12 @@ stem(n, x1), title('Question 4'); xlabel("x[n]"); ylabel("n");
 
 %% Question 5:
 % Plot the sine wave
-t = linspace(0,2,0.1)
-fs = 250;                % Sampling frequency
-dt = 1/fs;               % Seconds per sample
-StopTime = 1;            % 0.25 seconds
-t1 = (0:dt:StopTime-dt); % seconds
-F = 3;                   % 5,2 Sine wave frequency Nyquist theorem says fs must be at least F*2
+fs = 250;
+dt = 1/fs;
+StopTime = 2;
+t = (0:dt:StopTime-dt);
+F = 3;
 data = sin(2*pi*F*t);
+
+figure,
+plot(t,data), title('Question 5 - Bonus');
